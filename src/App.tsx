3558b9899ca6c1a0ffa1ebc5c1d9d6d740e9c3bc
houndhollis <houndhollis9797@gmail.com>
2025-03-07 00:25:39 +0900

    chore: import 변경 & 파일위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import ClientTestimonials from "./components/client-testimonials";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Main from "./components/main";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <ClientTestimonials />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
